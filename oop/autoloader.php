f59fe9014b0b3a13cf7e7cdb0532e15961b0a778
varunraj <varunvalsaraj@hotmail.com>
2015-09-23 21:49:02 +0400

    added autoload

---
+++ oop/autoloader.php
@@ -0,0 +1,27 @@
+<?php
+
+function autoLoader($className){
+
+
+$directories = array( '', 'test/');
+
+$fileformats = array('%s.php', '%s.class.php', 'class.%s.php', '%s.inc.php');
+
+foreach ($directories as $directory) {
+	foreach($fileformats as $fileformat){
+		 $path = $directory.sprintf($fileformat, $className);
+		 if(file_exists($path)){
+		 	include_once $path;
+		 	return;
+		 }
+	}
+}
+
+
+}
+spl_autoload_register('autoLoader');
+
+
+
+
+?>

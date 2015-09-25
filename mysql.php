9a2cac2a784c45aea8c30156b694e3904510ed6f
varunraj <varunvalsaraj@hotmail.com>
2015-09-25 12:56:55 +0400

    function to connect mysql database

--- mysql.php
+++ mysql.php
@@ -3,19 +3,24 @@ $host = 'localhost';
 $dbUser = 'varunblack';
 $dbPass = 'doublebarrel';
 $dbbase = 'social';
-if (!$dbconn=mysql_connect($host, $dbUser, $dbPass)){
-	die('could not connect to the server');
+
+function &connectToDb($host, $dbUser, $dbPass, $dbbase){
+if (!$dbconn=mysqli_connect($host, $dbUser, $dbPass)){
+	return false;
 }
 //select the database
-if(!mysql_select_db($dbbase, $dbconn)){
-	die('could not select the database');
+if(!mysqli_select_db($dbconn, $dbbase)){
+	return false;
 }
-echo "connection successfull!!";
 //close the connection when finished
 
-mysql_close($dbconn);
+return $dbconn;
 
-
-
+}
+$DBconn = &connecttoDb($host, $dbUser, $dbPass, $dbbase);
+$sql = 'SELECT * FROM user_data';
+$result = mysqli_query($DBconn, $sql);
+$row = mysqli_fetch_row($result);
+echo $row[0];
 
 ?>
